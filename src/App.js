11e32b2f4b2f1534cd39b0f1cb61804307d6a273
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2023-01-01 18:14:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  let str = '';
+
+  const newClicked = (event) => {
+    newOnpress(event.target.value);
+  }
+
+  const [onPress, newOnpress] = useState(str);
+
+  return (
+    <div className='maindiv'>
+      <div className='textarea'>
+        <textarea placeholder='-- Input Area --' onChange={newClicked}></textarea>
+      </div>
+      <div className='markdownarea'>
+        <ReactMarkdown>
+          {onPress}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+}
+
+export default App;
